31fcdb19d0ea3d85a7f7d860db75c6a00d9ab564
Minjae-An <mj3242@naver.com>
2023-11-20 18:30:14 +0900

    feat(MemberRequestDTO) : 회원 관련 요청 DTO 클래스 정의
    회원 관련 요청 DTO들의 모아놓는 클래스. 회원 가입시 사용되는
    JoinDto 정의

---
+++ spring/spring/src/main/java/umc/spring/web/dto/MemberRequestDTO.java
@@ -0,0 +1,30 @@
+package umc.spring.web.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.Getter;
+import umc.spring.domain.FoodCategory;
+import umc.spring.validation.annotation.ExistCategories;
+
+public class MemberRequestDTO {
+    @Getter
+    public static class JoinDto{
+        @NotBlank
+        String name;
+        @NotNull
+        Integer gender;
+        @NotNull
+        Integer birthYear;
+        @NotNull
+        Integer birthMonth;
+        @NotNull
+        Integer birthDay;
+        @Size(min = 5, max = 12)
+        String address;
+        @Size(min = 5, max = 12)
+        String specAddress;
+        @ExistCategories
+        FoodCategory foodCategory;
+    }
+}
