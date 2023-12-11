a9b51511cebdcd8d09fb05bdb5819698e40dd270
Minjae-An <mj3242@naver.com>
2023-12-11 21:48:36 +0900

    feat(ReviewConverter) : 미션 추가 관련 convert 메서드 구현(#3)

---
+++ spring/spring/src/main/java/umc/spring/converter/ReviewConverter.java
@@ -0,0 +1,36 @@
+package umc.spring.converter;
+
+import static umc.spring.web.dto.store.StoreRequestDTO.AddReviewRequestDTO;
+import static umc.spring.web.dto.store.StoreResponseDTO.AddReviewResponseDTO;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import umc.spring.domain.Review;
+import umc.spring.domain.ReviewImage;
+
+public class ReviewConverter {
+    public static Review toReview(AddReviewRequestDTO request) {
+        return Review.builder()
+                .title(request.getTitle())
+                .body(request.getBody())
+                .score(request.getScore())
+                .build();
+    }
+
+    public static List<ReviewImage> toReviewImages(Review review, List<String> imgUrls) {
+        return imgUrls.stream()
+                .map(imgUrl -> ReviewImage.builder()
+                        .imageUrl(imgUrl)
+                        .review(review)
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+    public static AddReviewResponseDTO toAddReviewResponseDTO(Review review) {
+        return AddReviewResponseDTO.builder()
+                .storeId(review.getStore().getId())
+                .reviewId(review.getId())
+                .createdAt(review.getCreatedAt())
+                .build();
+    }
+}
