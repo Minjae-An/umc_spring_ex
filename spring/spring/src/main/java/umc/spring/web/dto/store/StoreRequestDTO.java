146573852f71524cca53cdd170bf0e5a969ec58b
Minjae-An <mj3242@naver.com>
2023-12-11 21:51:41 +0900

    feat(StoreRequestDTO) : 리뷰 추가 요청 DTO 정의(#3)

--- spring/spring/src/main/java/umc/spring/web/dto/store/StoreRequestDTO.java
+++ spring/spring/src/main/java/umc/spring/web/dto/store/StoreRequestDTO.java
@@ -9,7 +9,7 @@ import org.hibernate.validator.constraints.Range;
 
 public class StoreRequestDTO {
     @Getter
-    public static class AddMissionRequestDTO{
+    public static class AddMissionRequestDTO {
         @NotNull
         @Range(max = 1_000) // default min = 0L
         private Integer reward;
@@ -18,5 +18,28 @@ public class StoreRequestDTO {
         @NotBlank
         @Length(min = 20, max = 100)
         private String missionSpec;
+    }
+
+    @Getter
+    public static class AddReviewRequestDTO {
+        @NotNull
+        private Long memberId;
+
+        @NotBlank
+        @Length(min = 1, max = 50)
+        private String title;
+
+        @NotBlank
+        @Length(min = 10, max = 200)
+        private String body;
+
+        @NotNull
+        @Range(min = 0, max = 5)
+        private Float score;
+
+        // reviewImage 관련 데이터는 S3가 관련되어 있어 편의상 배제하였다.
+//        @NotNull
+//        @Size(max = 3)
+//        List<MultipartFile> reviewImages;
     }
 }
