47ec92d2cca81d114a65eec4e405fe15a4d57dea
Minjae-An <mj3242@naver.com>
2023-12-11 18:54:58 +0900

    feat(ErrorStatus) : STORE_NOT_FOUND 코드 추가(#4)

--- spring/spring/src/main/java/umc/spring/apiPayload/code/status/ErrorStatus.java
+++ spring/spring/src/main/java/umc/spring/apiPayload/code/status/ErrorStatus.java
@@ -30,7 +30,10 @@ public enum ErrorStatus implements BaseErrorCode {
     FOOD_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "FOOD_CATEGORY4001", "음식 카테고리가 없습니다."),
 
     // Region relative exception
-    REGION_NOT_FOUND(HttpStatus.NOT_FOUND, "REGION4001", "지역이 존재하지 않습니다.");
+    REGION_NOT_FOUND(HttpStatus.NOT_FOUND, "REGION4001", "지역이 존재하지 않습니다."),
+
+    // Store relative exception
+    STORE_NOT_FOUND(HttpStatus.NOT_FOUND, "STORE4001", "가게가 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private String code;
