a1adffa99b1e3f6294643ae4ce1c17bc6fc9cffc
Minjae-An <mj3242@naver.com>
2023-12-11 21:50:24 +0900

    feat(StoreCommandService) : addReview 정의(#3)

--- spring/spring/src/main/java/umc/spring/service/store_service/StoreCommandService.java
+++ spring/spring/src/main/java/umc/spring/service/store_service/StoreCommandService.java
@@ -1,8 +1,10 @@
 package umc.spring.service.store_service;
 
 import umc.spring.domain.Mission;
+import umc.spring.domain.Review;
 import umc.spring.web.dto.store.StoreRequestDTO;
 
 public interface StoreCommandService {
     Mission addMission(StoreRequestDTO.AddMissionRequestDTO request, Long storeId);
+    Review addReview(StoreRequestDTO.AddReviewRequestDTO requestDTO, Long storeId);
 }
