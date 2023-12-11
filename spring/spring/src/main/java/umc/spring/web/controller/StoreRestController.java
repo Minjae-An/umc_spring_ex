3be813065bcd64c6e5bea8edf4462d5be545e136
Minjae-An <mj3242@naver.com>
2023-12-11 21:52:26 +0900

    feat(StoreRestController) : 리뷰 추가 API 구현(#3)

--- spring/spring/src/main/java/umc/spring/web/controller/StoreRestController.java
+++ spring/spring/src/main/java/umc/spring/web/controller/StoreRestController.java
@@ -10,8 +10,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import umc.spring.converter.MissionConverter;
+import umc.spring.converter.ReviewConverter;
 import umc.spring.domain.Mission;
+import umc.spring.domain.Review;
 import umc.spring.service.store_service.StoreCommandService;
+import umc.spring.validation.annotation.ExistStores;
 import umc.spring.web.dto.store.StoreRequestDTO;
 import umc.spring.web.dto.store.StoreResponseDTO;
 
@@ -24,9 +27,17 @@ public class StoreRestController {
 
     @PostMapping("/{storeId}/mission")
     public ResponseEntity<StoreResponseDTO.AddMissionResponseDTO>
-        addMission(@RequestBody @Valid StoreRequestDTO.AddMissionRequestDTO request,
-                   @PathVariable Long storeId){
+    addMission(@RequestBody @Valid StoreRequestDTO.AddMissionRequestDTO request,
+               @PathVariable Long storeId) {
         Mission mission = storeCommandService.addMission(request, storeId);
         return ResponseEntity.ok(MissionConverter.toAddMissionResponseDTO(mission));
+    }
+
+    @PostMapping("/{storeId}/review")
+    public ResponseEntity<StoreResponseDTO.AddReviewResponseDTO>
+    addReview(@RequestBody @Valid StoreRequestDTO.AddReviewRequestDTO request,
+              @PathVariable @ExistStores Long storeId) {
+        Review review = storeCommandService.addReview(request, storeId);
+        return ResponseEntity.ok(ReviewConverter.toAddReviewResponseDTO(review));
     }
 }
