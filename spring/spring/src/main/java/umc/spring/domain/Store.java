f33daaacff448eabe7ca4bf13186634ba89c4061
Minjae-An <mj3242@naver.com>
2023-12-11 18:02:21 +0900

    feat(Store) : Region 연관관계 편의 메서드 추가(#2)

--- spring/spring/src/main/java/umc/spring/domain/Store.java
+++ spring/spring/src/main/java/umc/spring/domain/Store.java
@@ -3,6 +3,7 @@ package umc.spring.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -18,6 +19,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 import umc.spring.domain.common.BaseEntity;
 import umc.spring.domain.mapping.MemberMission;
 
@@ -36,7 +38,7 @@ public class Store extends BaseEntity {
     @Column(nullable = false, length = 50)
     private String address;
 
-    @Column(nullable = false, columnDefinition = "FLOAT DEFAULT 0.0")
+    @ColumnDefault("0.0")
     private Float score;
 
     @OneToMany(mappedBy = "mission", cascade = CascadeType.ALL)
@@ -48,4 +50,13 @@ public class Store extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "region_id")
     private Region region;
+
+    // 연관 관계 편의 메서드
+    public void region(Region region) {
+        Optional.ofNullable(this.region)
+                .ifPresent(oldRegion -> oldRegion.removeStore(this));
+
+        this.region = region;
+        region.addStore(this);
+    }
 }
