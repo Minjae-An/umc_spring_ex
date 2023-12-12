975ba91ea692d35eb248f8756b4fa93281d51b1a
Minjae-An <mj3242@naver.com>
2023-12-12 15:28:44 +0900

    feat(MemberRestController) : 도전 중 미션 추가 API 구현(#5)

--- spring/spring/src/main/java/umc/spring/web/controller/MemberRestController.java
+++ spring/spring/src/main/java/umc/spring/web/controller/MemberRestController.java
@@ -1,5 +1,10 @@
 package umc.spring.web.controller;
 
+import static umc.spring.web.dto.MemberRequestDTO.ChallengeMissionRequestDto;
+import static umc.spring.web.dto.MemberRequestDTO.JoinDto;
+import static umc.spring.web.dto.MemberResponseDTO.ChallengeMissionResponseDto;
+import static umc.spring.web.dto.MemberResponseDTO.JoinResultDTO;
+
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
@@ -10,20 +15,27 @@ import org.springframework.web.bind.annotation.RestController;
 import umc.spring.apiPayload.ApiResponse;
 import umc.spring.converter.MemberConverter;
 import umc.spring.domain.Member;
+import umc.spring.domain.mapping.MemberMission;
 import umc.spring.service.member_service.MemberCommandService;
-import umc.spring.web.dto.MemberRequestDTO;
-import umc.spring.web.dto.MemberResponseDTO;
+import umc.spring.validation.annotation.OngoingMission;
 
 @RestController
 @RequiredArgsConstructor
 @Validated
-@RequestMapping("/members")
+@RequestMapping("/member")
 public class MemberRestController {
     private final MemberCommandService memberCommandService;
 
     @PostMapping("/")
-    public ApiResponse<MemberResponseDTO.JoinResultDTO> join(@RequestBody @Valid MemberRequestDTO.JoinDto request) {
+    public ApiResponse<JoinResultDTO> join(@RequestBody @Valid JoinDto request) {
         Member member = memberCommandService.joinMember(request);
         return ApiResponse.onSuccess(MemberConverter.toJoinResultDTO(member));
+    }
+
+    @PostMapping("/mission")
+    public ApiResponse<ChallengeMissionResponseDto>
+    challengeMission(@RequestBody @Valid @OngoingMission ChallengeMissionRequestDto request) {
+        MemberMission memberMission = memberCommandService.challengeMission(request);
+        return ApiResponse.onSuccess(MemberConverter.toChallengeMissionResponseDto(memberMission));
     }
 }
