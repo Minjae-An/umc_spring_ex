a677e2799fee2da4dd81f59f313a07b17fb0b5d0
Minjae-An <mj3242@naver.com>
2023-12-12 15:19:24 +0900

    feat(MemberCommandService) : findById, challengeMission 메서드 구현(#5)

--- spring/spring/src/main/java/umc/spring/service/member_service/MemberCommandService.java
+++ spring/spring/src/main/java/umc/spring/service/member_service/MemberCommandService.java
@@ -1,8 +1,15 @@
 package umc.spring.service.member_service;
 
+import static umc.spring.web.dto.MemberRequestDTO.ChallengeMissionRequestDto;
+import static umc.spring.web.dto.MemberRequestDTO.JoinDto;
+
 import umc.spring.domain.Member;
-import umc.spring.web.dto.MemberRequestDTO;
+import umc.spring.domain.mapping.MemberMission;
 
 public interface MemberCommandService {
-    Member joinMember(MemberRequestDTO.JoinDto request);
+    Member findById(Long memberId);
+
+    Member joinMember(JoinDto request);
+
+    MemberMission challengeMission(ChallengeMissionRequestDto request);
 }
