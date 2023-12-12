3e1fab188afa68da5c9bc5131b47bf81fd3bc78d
Minjae-An <mj3242@naver.com>
2023-12-12 15:25:16 +0900

    feat(MemberResponseDTO) : 도전중 미션 추가 응답 DTO 구현(#5)

--- spring/spring/src/main/java/umc/spring/web/dto/MemberResponseDTO.java
+++ spring/spring/src/main/java/umc/spring/web/dto/MemberResponseDTO.java
@@ -15,4 +15,13 @@ public class MemberResponseDTO {
         Long memberId;
         LocalDateTime createdAt;
     }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ChallengeMissionResponseDto{
+        Long memberId;
+        Long memberMissionId;
+    }
 }
