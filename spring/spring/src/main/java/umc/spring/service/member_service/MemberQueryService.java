c795d7325825d4847b52a17b77838b310349ebd8
Minjae-An <mj3242@naver.com>
2023-12-12 15:23:43 +0900

    feat(MemberQueryService) : 도전중 미션 판별 기능 정의(#5)
    해당 기능 구현에 필요한 findById 메서드 추가 정의

--- spring/spring/src/main/java/umc/spring/service/member_service/MemberQueryService.java
+++ spring/spring/src/main/java/umc/spring/service/member_service/MemberQueryService.java
@@ -1,4 +1,10 @@
 package umc.spring.service.member_service;
 
+import umc.spring.domain.Member;
+import umc.spring.web.dto.MemberRequestDTO.ChallengeMissionRequestDto;
+
 public interface MemberQueryService {
+    Member findById(Long memberId);
+
+    boolean isOngoingMission(ChallengeMissionRequestDto request);
 }
