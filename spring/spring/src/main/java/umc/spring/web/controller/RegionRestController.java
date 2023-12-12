c29e44d4c69bdd5990856047691ef1d1b7eb27bd
Minjae-An <mj3242@naver.com>
2023-12-12 15:26:45 +0900

    feat : 일관성 있는 응답을 위해 Controller API 응답, ApiResponse로 통일

--- spring/spring/src/main/java/umc/spring/web/controller/RegionRestController.java
+++ spring/spring/src/main/java/umc/spring/web/controller/RegionRestController.java
@@ -2,18 +2,18 @@ package umc.spring.web.controller;
 
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import umc.spring.apiPayload.ApiResponse;
 import umc.spring.converter.RegionConverter;
 import umc.spring.domain.Store;
 import umc.spring.service.region_service.RegionCommandService;
 import umc.spring.web.dto.region.RegionRequestDTO;
-import umc.spring.web.dto.region.RegionResponseDTO;
+import umc.spring.web.dto.region.RegionResponseDTO.AddStoreToRegionResponseDTO;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,10 +23,10 @@ public class RegionRestController {
     private final RegionCommandService regionCommandService;
 
     @PostMapping("/{regionId}/store")
-    public ResponseEntity<RegionResponseDTO.AddStoreToRegionResponseDTO>
+    public ApiResponse<AddStoreToRegionResponseDTO>
         addStoreToRegion(@RequestBody @Valid RegionRequestDTO.AddStoreToRegionRequestDTO request,
                 @PathVariable Long regionId){
         Store store = regionCommandService.addStoreToRegion(request, regionId);
-        return ResponseEntity.ok(RegionConverter.toAddStoreToRegionResponseDTO(store));
+        return ApiResponse.onSuccess(RegionConverter.toAddStoreToRegionResponseDTO(store));
     }
 }
--- spring/spring/src/main/java/umc/spring/web/controller/StoreRestController.java
+++ spring/spring/src/main/java/umc/spring/web/controller/StoreRestController.java
@@ -2,13 +2,13 @@ package umc.spring.web.controller;
 
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import umc.spring.apiPayload.ApiResponse;
 import umc.spring.converter.MissionConverter;
 import umc.spring.converter.ReviewConverter;
 import umc.spring.domain.Mission;
@@ -17,6 +17,7 @@ import umc.spring.service.store_service.StoreCommandService;
 import umc.spring.validation.annotation.ExistStores;
 import umc.spring.web.dto.store.StoreRequestDTO;
 import umc.spring.web.dto.store.StoreResponseDTO;
+import umc.spring.web.dto.store.StoreResponseDTO.AddMissionResponseDTO;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,18 +27,18 @@ public class StoreRestController {
     private final StoreCommandService storeCommandService;
 
     @PostMapping("/{storeId}/mission")
-    public ResponseEntity<StoreResponseDTO.AddMissionResponseDTO>
+    public ApiResponse<AddMissionResponseDTO>
     addMission(@RequestBody @Valid StoreRequestDTO.AddMissionRequestDTO request,
                @PathVariable Long storeId) {
         Mission mission = storeCommandService.addMission(request, storeId);
-        return ResponseEntity.ok(MissionConverter.toAddMissionResponseDTO(mission));
+        return ApiResponse.onSuccess(MissionConverter.toAddMissionResponseDTO(mission));
     }
 
     @PostMapping("/{storeId}/review")
-    public ResponseEntity<StoreResponseDTO.AddReviewResponseDTO>
+    public ApiResponse<StoreResponseDTO.AddReviewResponseDTO>
     addReview(@RequestBody @Valid StoreRequestDTO.AddReviewRequestDTO request,
               @PathVariable @ExistStores Long storeId) {
         Review review = storeCommandService.addReview(request, storeId);
-        return ResponseEntity.ok(ReviewConverter.toAddReviewResponseDTO(review));
+        return ApiResponse.onSuccess(ReviewConverter.toAddReviewResponseDTO(review));
     }
 }
