31f14bcfbebc4e773222374e31b02f1605c43e50
Minjae-An <mj3242@naver.com>
2023-12-13 20:41:08 +0900

    feat(StoreCommandServiceImpl) : Repository 의존성, Service로 대체

--- spring/spring/src/main/java/umc/spring/service/store_service/StoreCommandServiceImpl.java
+++ spring/spring/src/main/java/umc/spring/service/store_service/StoreCommandServiceImpl.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import umc.spring.apiPayload.code.status.ErrorStatus;
-import umc.spring.apiPayload.exception.handler.MemberHandler;
 import umc.spring.apiPayload.exception.handler.StoreHandler;
 import umc.spring.converter.MissionConverter;
 import umc.spring.converter.ReviewConverter;
@@ -14,11 +13,10 @@ import umc.spring.domain.Mission;
 import umc.spring.domain.Review;
 import umc.spring.domain.ReviewImage;
 import umc.spring.domain.Store;
-import umc.spring.repository.MemberRepository;
-import umc.spring.repository.MissionRepository;
-import umc.spring.repository.ReviewImageRepository;
-import umc.spring.repository.ReviewRepository;
 import umc.spring.repository.StoreRepository;
+import umc.spring.service.member_service.MemberQueryService;
+import umc.spring.service.mission_service.MissionCommandService;
+import umc.spring.service.review_service.ReviewCommandService;
 import umc.spring.web.dto.store.StoreRequestDTO.AddMissionRequestDTO;
 import umc.spring.web.dto.store.StoreRequestDTO.AddReviewRequestDTO;
 
@@ -27,10 +25,9 @@ import umc.spring.web.dto.store.StoreRequestDTO.AddReviewRequestDTO;
 @Transactional
 public class StoreCommandServiceImpl implements StoreCommandService {
     private final StoreRepository storeRepository;
-    private final MissionRepository missionRepository;
-    private final MemberRepository memberRepository;
-    private final ReviewRepository reviewRepository;
-    private final ReviewImageRepository reviewImageRepository;
+    private final MissionCommandService missionCommandService;
+    private final MemberQueryService memberQueryService;
+    private final ReviewCommandService reviewCommandService;
 
 
     @Override
@@ -43,7 +40,7 @@ public class StoreCommandServiceImpl implements StoreCommandService {
         Mission mission = MissionConverter.toMission(request);
         Store store = findById(storeId);
         mission.store(store);
-        return missionRepository.save(mission);
+        return missionCommandService.save(mission);
     }
 
     private Store findById(Long storeId) {
@@ -61,21 +58,11 @@ public class StoreCommandServiceImpl implements StoreCommandService {
         Store store = findById(storeId);
         review.store(store);
 
-        Member member = findMemberById(request.getMemberId());
+        Member member = memberQueryService.findById(request.getMemberId());
         review.member(member);
 
         List<String> exampleImgUrls = List.of("exampleImgUrl1", "exampleImgUrl2", "exampleImgUrl3");
         List<ReviewImage> reviewImages = ReviewConverter.toReviewImages(review, exampleImgUrls);
-        reviewImages.forEach(review::addReviewImage);
-        reviewImageRepository.saveAll(reviewImages);
-
-        return reviewRepository.save(review);
+        return reviewCommandService.save(review, reviewImages);
     }
-
-    private Member findMemberById(Long memberId) {
-        return memberRepository.findById(memberId)
-                .orElseThrow(() -> new MemberHandler(ErrorStatus.MEMBER_NOT_FOUND));
-    }
-
-
 }
