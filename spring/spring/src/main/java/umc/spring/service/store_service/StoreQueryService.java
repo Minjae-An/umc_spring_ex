150f5abb29f63496f7419c93f32b03017e3f1d71
Minjae-An <mj3242@naver.com>
2023-12-13 19:03:50 +0900

    feat(StoreQueryService) : 가게 조회 기능 정의

--- spring/spring/src/main/java/umc/spring/service/store_service/StoreQueryService.java
+++ spring/spring/src/main/java/umc/spring/service/store_service/StoreQueryService.java
@@ -1,5 +1,8 @@
 package umc.spring.service.store_service;
 
+import umc.spring.domain.Store;
+
 public interface StoreQueryService {
+    Store findById(Long storeId);
     boolean isStoreExist(Long storeId);
 }
