c1ba260788c151b87fe45cfbca2801447df67bca
Minjae-An <mj3242@naver.com>
2023-12-13 19:03:29 +0900

    feat(MemberCommandServiceImpl) : Repository에 대한 의존성, Service로 대체

--- spring/spring/src/main/java/umc/spring/service/member_service/MemberCommandServiceImpl.java
+++ spring/spring/src/main/java/umc/spring/service/member_service/MemberCommandServiceImpl.java
@@ -6,9 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import umc.spring.apiPayload.code.status.ErrorStatus;
-import umc.spring.apiPayload.exception.handler.FoodCategoryHandler;
 import umc.spring.apiPayload.exception.handler.MemberHandler;
-import umc.spring.apiPayload.exception.handler.MissionHandler;
 import umc.spring.converter.MemberConverter;
 import umc.spring.converter.MemberPreferConverter;
 import umc.spring.domain.FoodCategory;
@@ -16,9 +14,9 @@ import umc.spring.domain.Member;
 import umc.spring.domain.Mission;
 import umc.spring.domain.mapping.MemberMission;
 import umc.spring.domain.mapping.MemberPrefer;
-import umc.spring.repository.FoodCategoryRepository;
 import umc.spring.repository.MemberRepository;
-import umc.spring.repository.MissionRepository;
+import umc.spring.service.food_category_service.FoodCategoryQueryService;
+import umc.spring.service.mission_service.MissionQueryService;
 import umc.spring.web.dto.MemberRequestDTO.ChallengeMissionRequestDto;
 import umc.spring.web.dto.MemberRequestDTO.JoinDto;
 
@@ -27,13 +25,8 @@ import umc.spring.web.dto.MemberRequestDTO.JoinDto;
 @Transactional
 public class MemberCommandServiceImpl implements MemberCommandService {
     private final MemberRepository memberRepository;
-    private final FoodCategoryRepository foodCategoryRepository;
-    private final MissionRepository missionRepository;
-
-    private Mission findMissionById(Long missionId) {
-        return missionRepository.findById(missionId)
-                .orElseThrow(() -> new MissionHandler(ErrorStatus.MISSION_NOT_FOUND));
-    }
+    private final FoodCategoryQueryService foodCategoryQueryService;
+    private final MissionQueryService missionQueryService;
 
     @Override
     public Member findById(Long memberId) {
@@ -45,8 +38,7 @@ public class MemberCommandServiceImpl implements MemberCommandService {
     public Member joinMember(JoinDto request) {
         Member member = MemberConverter.toMember(request);
         List<FoodCategory> foodCategoryList = request.getPreferCategory().stream()
-                .map(category -> foodCategoryRepository.findById(category)
-                        .orElseThrow(() -> new FoodCategoryHandler(ErrorStatus.FOOD_CATEGORY_NOT_FOUND)))
+                .map(foodCategoryQueryService::findById)
                 .collect(Collectors.toList());
 
         List<MemberPrefer> memberPreferList = MemberPreferConverter.toMemberPreferList(foodCategoryList);
@@ -58,7 +50,7 @@ public class MemberCommandServiceImpl implements MemberCommandService {
     @Override
     public MemberMission challengeMission(ChallengeMissionRequestDto request) {
         Member member = findById(request.getMemberId());
-        Mission mission = findMissionById(request.getMissionId());
+        Mission mission = missionQueryService.findById(request.getMissionId());
         MemberMission memberMission = MemberConverter.toMemberMission(member, mission);
 
         member.challengeMission(memberMission);
