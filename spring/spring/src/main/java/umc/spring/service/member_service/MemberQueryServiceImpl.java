61ad7bf347d6be1085b7191789d1174b536d9d26
Minjae-An <mj3242@naver.com>
2023-12-13 18:55:27 +0900

    feat(MemberQueryServiceImpl) : MissionRepository 의존성, MissionQueryService 의존성으로 대체
    중복 코드 제거를 위해 Repository에 대한 의존성을 Service에 대한 의존성으로 대체

--- spring/spring/src/main/java/umc/spring/service/member_service/MemberQueryServiceImpl.java
+++ spring/spring/src/main/java/umc/spring/service/member_service/MemberQueryServiceImpl.java
@@ -5,11 +5,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import umc.spring.apiPayload.code.status.ErrorStatus;
 import umc.spring.apiPayload.exception.handler.MemberHandler;
-import umc.spring.apiPayload.exception.handler.MissionHandler;
 import umc.spring.domain.Member;
 import umc.spring.domain.Mission;
 import umc.spring.repository.MemberRepository;
-import umc.spring.repository.MissionRepository;
+import umc.spring.service.mission_service.MissionQueryService;
 import umc.spring.web.dto.MemberRequestDTO.ChallengeMissionRequestDto;
 
 @Service
@@ -17,7 +16,7 @@ import umc.spring.web.dto.MemberRequestDTO.ChallengeMissionRequestDto;
 @Transactional(readOnly = true)
 public class MemberQueryServiceImpl implements MemberQueryService {
     private final MemberRepository memberRepository;
-    private final MissionRepository missionRepository;
+    private final MissionQueryService missionQueryService;
 
     @Override
     public Member findById(Long memberId) {
@@ -30,15 +29,10 @@ public class MemberQueryServiceImpl implements MemberQueryService {
                 .anyMatch(memberMission -> memberMission.getMission().equals(mission));
     }
 
-    private Mission findMissionById(Long missionId) {
-        return missionRepository.findById(missionId)
-                .orElseThrow(() -> new MissionHandler(ErrorStatus.MISSION_NOT_FOUND));
-    }
-
     @Override
     public boolean isOngoingMission(ChallengeMissionRequestDto request) {
         Member member = findById(request.getMemberId());
-        Mission mission = findMissionById(request.getMissionId());
+        Mission mission = missionQueryService.findById(request.getMissionId());
         return hasMission(member, mission);
     }
 }
