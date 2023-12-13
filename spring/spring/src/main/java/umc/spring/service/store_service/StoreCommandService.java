a1663323f748fb2c5f32fe3aa7d99a64f4545ce9
Minjae-An <mj3242@naver.com>
2023-12-13 19:08:22 +0900

    feat(StoreCommandService) : 가게 정보 저장 기능 정의

--- spring/spring/src/main/java/umc/spring/service/store_service/StoreCommandService.java
+++ spring/spring/src/main/java/umc/spring/service/store_service/StoreCommandService.java
@@ -2,9 +2,11 @@ package umc.spring.service.store_service;
 
 import umc.spring.domain.Mission;
 import umc.spring.domain.Review;
+import umc.spring.domain.Store;
 import umc.spring.web.dto.store.StoreRequestDTO;
 
 public interface StoreCommandService {
+    Store save(Store store);
     Mission addMission(StoreRequestDTO.AddMissionRequestDTO request, Long storeId);
     Review addReview(StoreRequestDTO.AddReviewRequestDTO requestDTO, Long storeId);
 }
