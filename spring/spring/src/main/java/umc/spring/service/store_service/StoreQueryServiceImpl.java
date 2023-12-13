4a66a3f9ae939c9bb18840acfbfea48077752d0e
Minjae-An <mj3242@naver.com>
2023-12-13 19:04:08 +0900

    feat(StoreQueryServiceImpl) : 가게 조회 기능 구현

--- spring/spring/src/main/java/umc/spring/service/store_service/StoreQueryServiceImpl.java
+++ spring/spring/src/main/java/umc/spring/service/store_service/StoreQueryServiceImpl.java
@@ -3,6 +3,9 @@ package umc.spring.service.store_service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import umc.spring.apiPayload.code.status.ErrorStatus;
+import umc.spring.apiPayload.exception.handler.StoreHandler;
+import umc.spring.domain.Store;
 import umc.spring.repository.StoreRepository;
 
 @Service
@@ -10,6 +13,12 @@ import umc.spring.repository.StoreRepository;
 @Transactional(readOnly = true)
 public class StoreQueryServiceImpl implements StoreQueryService {
     private final StoreRepository storeRepository;
+
+    @Override
+    public Store findById(Long storeId) {
+        return storeRepository.findById(storeId)
+                .orElseThrow(() -> new StoreHandler(ErrorStatus.STORE_NOT_FOUND));
+    }
 
     @Override
     public boolean isStoreExist(Long storeId) {
