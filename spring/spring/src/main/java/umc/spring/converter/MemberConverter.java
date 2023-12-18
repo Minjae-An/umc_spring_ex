08656e7312693ad06ebbc3f665bb30f6ceb140b6
Minjae-An <mj3242@naver.com>
2023-12-18 20:11:54 +0900

    feat(MemberConverter) : 작성 리뷰 목록 조회 관련 DTO 매핑 메서드 구현(#7)

--- spring/spring/src/main/java/umc/spring/converter/MemberConverter.java
+++ spring/spring/src/main/java/umc/spring/converter/MemberConverter.java
@@ -4,12 +4,18 @@ import static umc.spring.web.dto.member.MemberResponseDTO.ChallengeMissionRespon
 import static umc.spring.web.dto.member.MemberResponseDTO.JoinResultDTO;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.data.domain.Page;
 import umc.spring.domain.Member;
 import umc.spring.domain.Mission;
+import umc.spring.domain.Review;
 import umc.spring.domain.enums.Gender;
 import umc.spring.domain.enums.MissionStatus;
 import umc.spring.domain.mapping.MemberMission;
 import umc.spring.web.dto.member.MemberRequestDTO;
+import umc.spring.web.dto.member.MemberResponseDTO.WrittenReviewResponseDto;
+import umc.spring.web.dto.page.PageResponseDto;
 
 public class MemberConverter {
     public static JoinResultDTO toJoinResultDTO(Member member) {
@@ -56,6 +62,31 @@ public class MemberConverter {
                 .status(MissionStatus.CHALLENGING)
                 .member(member)
                 .mission(mission)
+                .build();
+    }
+
+    public static WrittenReviewResponseDto writtenReviewResponseDto(Review review) {
+        return WrittenReviewResponseDto.builder()
+                .storeName(review.getStore().getName())
+                .memberName(review.getMember().getName())
+                .score(review.getScore())
+                .body(review.getBody())
+                .createdAt(review.getCreatedAt().toLocalDate())
+                .build();
+    }
+
+    public static PageResponseDto<WrittenReviewResponseDto> writtenReviewPageResponseDto(Page<Review> page) {
+        List<WrittenReviewResponseDto> contents = page.getContent().stream()
+                .map(MemberConverter::writtenReviewResponseDto)
+                .collect(Collectors.toList());
+
+        return PageResponseDto.<WrittenReviewResponseDto>builder()
+                .contents(contents)
+                .pageSize(page.getSize())
+                .totalPage(page.getTotalPages())
+                .totalElements(page.getTotalElements())
+                .isFirst(page.isFirst())
+                .isLast(page.isLast())
                 .build();
     }
 }
