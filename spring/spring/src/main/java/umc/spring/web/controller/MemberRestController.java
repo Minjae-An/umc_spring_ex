b25e74abcccb5a3d88bfd7eec04a796b9e743b8a
Minjae-An <mj3242@naver.com>
2023-12-18 20:14:14 +0900

    feat(MemberRestController) : 작성 리뷰 목록 조회 API 정의 & 명세(#7)

--- spring/spring/src/main/java/umc/spring/web/controller/MemberRestController.java
+++ spring/spring/src/main/java/umc/spring/web/controller/MemberRestController.java
@@ -5,12 +5,22 @@ import static umc.spring.web.dto.member.MemberRequestDTO.JoinDto;
 import static umc.spring.web.dto.member.MemberResponseDTO.ChallengeMissionResponseDto;
 import static umc.spring.web.dto.member.MemberResponseDTO.JoinResultDTO;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import javax.validation.Valid;
+import javax.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import umc.spring.apiPayload.ApiResponse;
 import umc.spring.converter.MemberConverter;
@@ -18,6 +28,9 @@ import umc.spring.domain.Member;
 import umc.spring.domain.mapping.MemberMission;
 import umc.spring.service.member_service.MemberCommandService;
 import umc.spring.validation.annotation.OngoingMission;
+import umc.spring.web.dto.member.MemberResponseDTO.WrittenReviewResponseDto;
+import umc.spring.web.dto.page.PageRequestDto;
+import umc.spring.web.dto.page.PageResponseDto;
 
 @RestController
 @RequiredArgsConstructor
@@ -37,5 +50,31 @@ public class MemberRestController {
     challengeMission(@RequestBody @Valid @OngoingMission ChallengeMissionRequestDto request) {
         MemberMission memberMission = memberCommandService.challengeMission(request);
         return ApiResponse.onSuccess(MemberConverter.toChallengeMissionResponseDto(memberMission));
+    }
+
+    @GetMapping("{memberId}/written-reviews")
+    @Operation(
+            summary = "해당 멤버가 작성한 리뷰 목록 조회 API",
+            description = "특정 멤버가 작성한 리뷰 목록을 조회하는 API이며, 페이징을 포함합니다."
+                    + "query string으로 page 번호를 주세요(size는 선택, 기본 10)")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH003", description = "access 토큰을 주세요!",
+                    content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH004", description = "acess 토큰 만료",
+                    content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH006", description = "acess 토큰 모양이 이상함",
+                    content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+    })
+    @Parameters({
+            @Parameter(name = "memberId", description = "멤버의 아이디, path variable"),
+            @Parameter(name = "page", description = "페이지 번호, query string"),
+            @Parameter(name = "size", description = "(선택적) 페이지당 컨텐츠 수, query string")
+    })
+    public ApiResponse<PageResponseDto<WrittenReviewResponseDto>>
+    getWrittenReviews(@PathVariable @PositiveOrZero Long memberId,
+                      @RequestParam @Valid PageRequestDto request) {
+
+        return null;
     }
 }
