b5b66621843ceacc98d89d800768aaf8a5cd34b6
Minjae-An <mj3242@naver.com>
2023-12-18 18:26:11 +0900

    feat(StoreResponseDTO) : 가게 리뷰 목록 조회 관련 응답 DTO 구현(#6)

--- spring/spring/src/main/java/umc/spring/web/dto/store/StoreResponseDTO.java
+++ spring/spring/src/main/java/umc/spring/web/dto/store/StoreResponseDTO.java
@@ -1,6 +1,8 @@
 package umc.spring.web.dto.store;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,7 +13,7 @@ public class StoreResponseDTO {
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
-    public static class AddMissionResponseDTO{
+    public static class AddMissionResponseDTO {
         private Long storeId;
         private Long missionId;
         private LocalDateTime createdAt;
@@ -21,9 +23,34 @@ public class StoreResponseDTO {
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
-    public static class AddReviewResponseDTO{
+    public static class AddReviewResponseDTO {
         private Long storeId;
         private Long reviewId;
         private LocalDateTime createdAt;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewPreviewListDTO {
+        private List<ReviewPreviewDTO> reviews;
+        private Integer listSize;
+        private Integer totalPage;
+        private Long totalElements;
+        private Boolean isFirst;
+        private Boolean isLast;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewPreviewDTO {
+        // Member에 대한 정보가 고도화되어 필요한 경우 별도의 DTO로 표현 권장
+        private String ownerNickname;
+        private Float score;
+        private String body;
+        private LocalDate createdAt;
     }
 }
