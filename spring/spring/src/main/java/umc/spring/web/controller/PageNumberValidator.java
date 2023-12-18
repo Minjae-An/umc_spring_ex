5bd4106f9c9e808deb3c41e3697da2c165825a2c
Minjae-An <mj3242@naver.com>
2023-12-18 20:09:57 +0900

    feat(PageNumberValidator) : 페이지 번호 검증 기능 구현(#7)

---
+++ spring/spring/src/main/java/umc/spring/web/controller/PageNumberValidator.java
@@ -0,0 +1,28 @@
+package umc.spring.web.controller;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import org.springframework.stereotype.Component;
+import umc.spring.apiPayload.code.status.ErrorStatus;
+import umc.spring.validation.annotation.CheckPage;
+
+@Component
+public class PageNumberValidator implements ConstraintValidator<CheckPage, Integer> {
+    @Override
+    public void initialize(CheckPage constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(Integer value, ConstraintValidatorContext context) {
+        boolean isValid = value < 0;
+
+        if (!isValid) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(ErrorStatus.PAGE_NUMBER_OUT_OF_RANGE.toString())
+                    .addConstraintViolation();
+        }
+
+        return isValid;
+    }
+}
