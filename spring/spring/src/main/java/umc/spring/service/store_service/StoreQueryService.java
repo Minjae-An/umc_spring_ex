9c207c0a88706388a3096a631ece6cbdb66a1bdf
Minjae-An <mj3242@naver.com>
2023-12-18 18:23:12 +0900

    feat(StoreQueryService) : 가게별 리뷰 목록 조회 기능 정의(#6)

--- spring/spring/src/main/java/umc/spring/service/store_service/StoreQueryService.java
+++ spring/spring/src/main/java/umc/spring/service/store_service/StoreQueryService.java
@@ -1,8 +1,11 @@
 package umc.spring.service.store_service;
 
+import org.springframework.data.domain.Page;
+import umc.spring.domain.Review;
 import umc.spring.domain.Store;
 
 public interface StoreQueryService {
     Store findById(Long storeId);
     boolean isStoreExist(Long storeId);
+    Page<Review> getReviewList(Long storeId, Integer page);
 }
