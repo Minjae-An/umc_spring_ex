3a341db3de745955ac2544878103850346476cbc
Minjae-An <mj3242@naver.com>
2023-12-18 20:09:36 +0900

    feat(CheckPage) : 페이지 번호 검증 어노테이션 구현(#7)

---
+++ spring/spring/src/main/java/umc/spring/validation/annotation/CheckPage.java
@@ -0,0 +1,20 @@
+package umc.spring.validation.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import umc.spring.web.controller.PageNumberValidator;
+
+@Documented
+@Constraint(validatedBy = PageNumberValidator.class)
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CheckPage {
+    String message() default "페이지 번호는 1이상이어야 합니다.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
