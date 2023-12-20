0d4d49c439682c0ce3f1ce23cbcd97aa32e268bf
Minjae-An <mj3242@naver.com>
2023-12-20 17:03:03 +0900

    feat(MemberQueryServiceImpl) : 멤버가 작성한 리뷰 조회 기능 구현(#7)

--- spring/spring/src/main/java/umc/spring/service/member_service/MemberQueryServiceImpl.java
+++ spring/spring/src/main/java/umc/spring/service/member_service/MemberQueryServiceImpl.java
@@ -1,14 +1,18 @@
 package umc.spring.service.member_service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import umc.spring.apiPayload.code.status.ErrorStatus;
 import umc.spring.apiPayload.exception.handler.MemberHandler;
 import umc.spring.domain.Member;
 import umc.spring.domain.Mission;
+import umc.spring.domain.Review;
 import umc.spring.repository.MemberRepository;
 import umc.spring.service.mission_service.MissionQueryService;
+import umc.spring.service.review_service.ReviewQueryService;
 import umc.spring.web.dto.member.MemberRequestDTO.ChallengeMissionRequestDto;
 
 @Service
@@ -17,6 +21,7 @@ import umc.spring.web.dto.member.MemberRequestDTO.ChallengeMissionRequestDto;
 public class MemberQueryServiceImpl implements MemberQueryService {
     private final MemberRepository memberRepository;
     private final MissionQueryService missionQueryService;
+    private final ReviewQueryService reviewQueryService;
 
     @Override
     public Member findById(Long memberId) {
@@ -34,5 +39,11 @@ public class MemberQueryServiceImpl implements MemberQueryService {
         Member member = findById(request.getMemberId());
         Mission mission = missionQueryService.findById(request.getMissionId());
         return hasMission(member, mission);
+    }
+
+    @Override
+    public Page<Review> getWrittenReviews(Long memberId, PageRequest request) {
+        Member member = findById(memberId);
+        return reviewQueryService.findAllByMember(member, request);
     }
 }
