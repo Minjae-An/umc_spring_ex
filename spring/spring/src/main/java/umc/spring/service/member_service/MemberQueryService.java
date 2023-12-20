5545496681e1a15aceef1a159caef9400b08151e
Minjae-An <mj3242@naver.com>
2023-12-20 17:01:51 +0900

    feat(MemberQueryService) : 멤버가 작성한 리뷰 조회 기능 정의(#7)

--- spring/spring/src/main/java/umc/spring/service/member_service/MemberQueryService.java
+++ spring/spring/src/main/java/umc/spring/service/member_service/MemberQueryService.java
@@ -1,14 +1,15 @@
 package umc.spring.service.member_service;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import umc.spring.domain.Member;
 import umc.spring.domain.Review;
 import umc.spring.web.dto.member.MemberRequestDTO.ChallengeMissionRequestDto;
-import umc.spring.web.dto.page.PageRequestDto;
 
 public interface MemberQueryService {
     Member findById(Long memberId);
 
     boolean isOngoingMission(ChallengeMissionRequestDto request);
-    Page<Review> getWrittenReviews(Long memberId, PageRequestDto request);
+
+    Page<Review> getWrittenReviews(Long memberId, PageRequest request);
 }
