0a49887bd7ed7f42f08b1f99a2fb0d175e91a9b3
Minjae-An <mj3242@naver.com>
2023-12-21 14:01:35 +0900

    feat(MissionQueryService) : 가게 미션 조회 기능(페이징 포함) 정의(#8)

--- spring/spring/src/main/java/umc/spring/service/mission_service/MissionQueryService.java
+++ spring/spring/src/main/java/umc/spring/service/mission_service/MissionQueryService.java
@@ -1,7 +1,12 @@
 package umc.spring.service.mission_service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import umc.spring.domain.Mission;
+import umc.spring.domain.Store;
 
 public interface MissionQueryService {
     Mission findById(Long missionId);
+
+    Page<Mission> findAllByStore(Store store, PageRequest pageRequest);
 }
