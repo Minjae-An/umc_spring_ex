387271c47a73194bdbb5506bbf08b0c524e0065b
Minjae-An <mj3242@naver.com>
2023-12-21 14:03:55 +0900

    feat(StoreRestController) : 가게 미션 조회 API 명세, 구현(#8)
    해결 : close #8

--- spring/spring/src/main/java/umc/spring/web/controller/StoreRestController.java
+++ spring/spring/src/main/java/umc/spring/web/controller/StoreRestController.java
@@ -7,8 +7,10 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import javax.validation.Valid;
+import javax.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,16 +21,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import umc.spring.apiPayload.ApiResponse;
 import umc.spring.converter.MissionConverter;
+import umc.spring.converter.PageDtoConverter;
 import umc.spring.converter.ReviewConverter;
 import umc.spring.converter.StoreConverter;
 import umc.spring.domain.Mission;
 import umc.spring.domain.Review;
 import umc.spring.service.store_service.StoreCommandService;
 import umc.spring.service.store_service.StoreQueryService;
+import umc.spring.validation.annotation.CheckPage;
 import umc.spring.validation.annotation.ExistStores;
+import umc.spring.web.dto.page.PageResponseDto;
 import umc.spring.web.dto.store.StoreRequestDTO;
 import umc.spring.web.dto.store.StoreResponseDTO;
 import umc.spring.web.dto.store.StoreResponseDTO.AddMissionResponseDTO;
+import umc.spring.web.dto.store.StoreResponseDTO.MissionPreviewDTO;
 
 @RestController
 @RequiredArgsConstructor
@@ -77,4 +83,38 @@ public class StoreRestController {
         Page<Review> reviewPage = storeQueryService.getReviewList(storeId, page);
         return ApiResponse.onSuccess(StoreConverter.reviewPreviewListDTO(reviewPage));
     }
+
+
+    @GetMapping("/{storeId}/missions")
+    @Operation(
+            summary = "해당 가게의 미션 목록 조회 API",
+            description = "특정 가게의 미션 목록을 조회하는 API, 페이징을 포함한다."
+                    + "query String으로 page 번호, 페이지 사이즈(size, 선택적 기본 10) 필요")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH003", description = "access 토큰을 주세요!",
+                    content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH004", description = "acess 토큰 만료",
+                    content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH006", description = "acess 토큰 모양이 이상함",
+                    content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "STORE4001", description = "해당 id를 가진 Store가 존재하지 않음",
+                    content = @Content(schema = @Schema(implementation = ApiResponse.class)))
+    })
+    @Parameters({
+            @Parameter(name = "storeId", description = "가게의 아이디, path variable"),
+            @Parameter(name = "page", description = "페이지 번호, query string"),
+            @Parameter(name = "size", description = "(선택적) 페이지당 컨텐츠 수, query string")
+    })
+    public ApiResponse<PageResponseDto<StoreResponseDTO.MissionPreviewDTO>>
+    getMissions(@PathVariable @Positive Long storeId,
+                @RequestParam(name = "page") @CheckPage Integer page,
+                @RequestParam(name = "size", required = false) Integer size) {
+        PageRequest pageRequest = PageDtoConverter.toPageRequest(page, size);
+        Page<Mission> missions = storeQueryService.getMissionList(storeId, pageRequest);
+        PageResponseDto<MissionPreviewDTO> response =
+                StoreConverter.missionPreviewDTOPageResponseDto(missions);
+        return ApiResponse.onSuccess(response);
+    }
+
 }
