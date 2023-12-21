1a4c25c95e183320b74bcc3ff2fc13d1003efb15
Minjae-An <mj3242@naver.com>
2023-12-21 14:53:29 +0900

    feat(MemberQueryService) : 진행 중 미션 조회 기능 정의(#9)

--- spring/spring/src/main/java/umc/spring/service/member_service/MemberQueryService.java
+++ spring/spring/src/main/java/umc/spring/service/member_service/MemberQueryService.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import umc.spring.domain.Member;
 import umc.spring.domain.Review;
+import umc.spring.domain.mapping.MemberMission;
 import umc.spring.web.dto.member.MemberRequestDTO.ChallengeMissionRequestDto;
 
 public interface MemberQueryService {
@@ -12,4 +13,6 @@ public interface MemberQueryService {
     boolean isOngoingMission(ChallengeMissionRequestDto request);
 
     Page<Review> getWrittenReviews(Long memberId, PageRequest request);
+
+    Page<MemberMission> getOngoingMissions(Long memberId, PageRequest request);
 }
