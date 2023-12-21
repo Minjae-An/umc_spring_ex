b86741c620ae364e69adebb1f0e795de9ac3f35b
Minjae-An <mj3242@naver.com>
2023-12-21 14:55:10 +0900

    feat(MemberRestController) : 진행 중 미션 조회 기능 API 구현(#9)

--- spring/spring/src/main/java/umc/spring/web/controller/MemberRestController.java
+++ spring/spring/src/main/java/umc/spring/web/controller/MemberRestController.java
@@ -12,7 +12,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import javax.validation.Valid;
-import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import umc.spring.apiPayload.ApiResponse;
 import umc.spring.converter.MemberConverter;
@@ -31,7 +32,9 @@ import umc.spring.domain.Review;
 import umc.spring.domain.mapping.MemberMission;
 import umc.spring.service.member_service.MemberCommandService;
 import umc.spring.service.member_service.MemberQueryService;
+import umc.spring.validation.annotation.CheckPage;
 import umc.spring.validation.annotation.OngoingMission;
+import umc.spring.web.dto.member.MemberResponseDTO.OngoingMissionResponseDto;
 import umc.spring.web.dto.member.MemberResponseDTO.WrittenReviewResponseDto;
 import umc.spring.web.dto.page.PageRequestDto;
 import umc.spring.web.dto.page.PageResponseDto;
@@ -78,12 +81,42 @@ public class MemberRestController {
             @Parameter(name = "size", description = "(선택적) 페이지당 컨텐츠 수, query string")
     })
     public ApiResponse<PageResponseDto<WrittenReviewResponseDto>>
-    getWrittenReviews(@PathVariable @PositiveOrZero Long memberId,
+    getWrittenReviews(@PathVariable @Positive Long memberId,
                       @Valid PageRequestDto request) {
         PageRequest pageRequest = PageDtoConverter.toPageRequest(request);
         Page<Review> page = memberQueryService.getWrittenReviews(memberId, pageRequest);
         PageResponseDto<WrittenReviewResponseDto> response =
                 MemberConverter.writtenReviewPageResponseDto(page);
+        return ApiResponse.onSuccess(response);
+    }
+
+    @GetMapping("/{memberId}/ongoing-mission")
+    @Operation(
+            summary = "해당 멤버가 진행 중인 미션 목록 조회 API",
+            description = "특정 멤버가 진행 중인 미션 목록을 조회하는 API이며, 페이징을 포함합니다."
+                    + "query string으로 page 번호를 주세요(size는 선택, 기본 10)"
+    )
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH003", description = "access 토큰을 주세요!",
+                    content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH004", description = "acess 토큰 만료",
+                    content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH006", description = "acess 토큰 모양이 이상함",
+                    content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "MEMBER4001", description = "해당 멤버가 존재하지 않음",
+                    content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "PAGE4001", description = "페이지 번호는 1이상이어야 함",
+                    content = @Content(schema = @Schema(implementation = ApiResponse.class)))
+    })
+    public ApiResponse<PageResponseDto<OngoingMissionResponseDto>>
+    getOngoingMissions(@PathVariable @Positive Long memberId,
+                       @RequestParam(name = "page") @CheckPage Integer page,
+                       @RequestParam(name = "size", required = false) Integer size) {
+        PageRequest pageRequest = PageDtoConverter.toPageRequest(page, size);
+        Page<MemberMission> ongoingMissions = memberQueryService.getOngoingMissions(memberId, pageRequest);
+        PageResponseDto<OngoingMissionResponseDto> response =
+                MemberConverter.ongoingMissionPageResponseDto(ongoingMissions);
         return ApiResponse.onSuccess(response);
     }
 }
