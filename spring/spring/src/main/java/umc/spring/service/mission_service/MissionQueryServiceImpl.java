60f76c54b089f1d04147518ebc57b5cd585f9eb4
Minjae-An <mj3242@naver.com>
2023-12-21 14:02:01 +0900

    feat(MissionQueryServiceImpl) : 가게 미션 조회 기능 구현(#8)

--- spring/spring/src/main/java/umc/spring/service/mission_service/MissionQueryServiceImpl.java
+++ spring/spring/src/main/java/umc/spring/service/mission_service/MissionQueryServiceImpl.java
@@ -1,11 +1,14 @@
 package umc.spring.service.mission_service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import umc.spring.apiPayload.code.status.ErrorStatus;
 import umc.spring.apiPayload.exception.handler.MissionHandler;
 import umc.spring.domain.Mission;
+import umc.spring.domain.Store;
 import umc.spring.repository.MissionRepository;
 
 @Service
@@ -19,5 +22,10 @@ public class MissionQueryServiceImpl implements MissionQueryService {
     public Mission findById(Long missionId) {
         return missionRepository.findById(missionId)
                 .orElseThrow(() -> new MissionHandler(ErrorStatus.MISSION_NOT_FOUND));
+    }
+
+    @Override
+    public Page<Mission> findAllByStore(Store store, PageRequest pageRequest) {
+        return missionRepository.findAllByStore(store, pageRequest);
     }
 }
