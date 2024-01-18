4d074217c14bdfcb2c018d040a9927e37d22dc88
Minjae-An <mj3242@naver.com>
2024-01-18 21:03:13 +0900

    feat(StoreCommandService) : 한 리뷰 이미지를 가진 리뷰 생성 기능 정의(#11)

--- spring/spring/src/main/java/umc/spring/service/store_service/StoreCommandService.java
+++ spring/spring/src/main/java/umc/spring/service/store_service/StoreCommandService.java
@@ -4,9 +4,11 @@ import umc.spring.domain.Mission;
 import umc.spring.domain.Review;
 import umc.spring.domain.Store;
 import umc.spring.web.dto.store.StoreRequestDTO;
+import umc.spring.web.dto.store.StoreRequestDTO.ReviewDTO;
 
 public interface StoreCommandService {
     Store save(Store store);
     Mission addMission(StoreRequestDTO.AddMissionRequestDTO request, Long storeId);
     Review addReview(StoreRequestDTO.AddReviewRequestDTO requestDTO, Long storeId);
+    Review createReview(Long memberId, Long storeId, ReviewDTO request);
 }
