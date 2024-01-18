c88b0de90c0964bf088f8e79ebd5160e43aa5444
Minjae-An <mj3242@naver.com>
2024-01-18 21:01:30 +0900

    feat(AmazonS3Manager) : S3 파일 업로드 기능 구현(#11)
    - 파일 업로드 기능 구현
    - 고유 KeyName 부여 로직 구현
    - 파일 확장자 추출 기능 구현

---
+++ spring/spring/src/main/java/umc/spring/aws/AmazonS3Manager.java
@@ -0,0 +1,44 @@
+package umc.spring.aws;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import umc.spring.config.AmazonConfig;
+import umc.spring.domain.common.Uuid;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AmazonS3Manager {
+    private final AmazonS3 amazonS3;
+    private final AmazonConfig amazonConfig;
+
+    public String uploadFile(String keyName, MultipartFile file) {
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(file.getSize());
+        keyName = keyName.concat(extractExtension(file.getOriginalFilename()));
+
+        try {
+            amazonS3.putObject(
+                    new PutObjectRequest(amazonConfig.getBucket(), keyName,
+                            file.getInputStream(), metadata));
+        } catch (IOException e) {
+            log.error("error at AmazonS3Manager uploadFile : {}", (Object) e.getStackTrace());
+        }
+
+        return amazonS3.getUrl(amazonConfig.getBucket(), keyName).toString();
+    }
+
+    public String generateReviewKeyName(Uuid uuid) {
+        return String.join("/", amazonConfig.getReviewPath(), uuid.getUuid());
+    }
+
+    private String extractExtension(String filename) {
+        return filename.substring(filename.lastIndexOf('.'));
+    }
+}
