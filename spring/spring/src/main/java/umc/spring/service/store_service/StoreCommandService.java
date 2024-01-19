b171ebe0c9bffdbccd025cde41606708873b822a
Minjae-An <mj3242@naver.com>
2024-01-19 14:21:58 +0900

    feat(StoreCommandService) : 리뷰 삭제 기능 정의(#11)

--- spring/spring/src/main/java/umc/spring/service/store_service/StoreCommandService.java
+++ spring/spring/src/main/java/umc/spring/service/store_service/StoreCommandService.java
@@ -5,10 +5,16 @@ import umc.spring.domain.Review;
 import umc.spring.domain.Store;
 import umc.spring.web.dto.store.StoreRequestDTO;
 import umc.spring.web.dto.store.StoreRequestDTO.ReviewDTO;
+import umc.spring.web.dto.store.StoreResponseDTO.DeleteReviewDTO;
 
 public interface StoreCommandService {
     Store save(Store store);
+
     Mission addMission(StoreRequestDTO.AddMissionRequestDTO request, Long storeId);
+
     Review addReview(StoreRequestDTO.AddReviewRequestDTO requestDTO, Long storeId);
+
     Review createReview(Long memberId, Long storeId, ReviewDTO request);
+
+    DeleteReviewDTO deleteReview(Long reviewId);
 }
