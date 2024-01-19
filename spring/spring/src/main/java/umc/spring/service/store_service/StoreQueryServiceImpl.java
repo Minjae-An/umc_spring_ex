57fbf1fb91795ccb23cfe56b3e5b2e4a82866ce7
Minjae-An <mj3242@naver.com>
2024-01-19 14:21:30 +0900

    feat(StoreQueryServiceImpl) : 리뷰 상세 내용 조회 기능 구현(#11)

--- spring/spring/src/main/java/umc/spring/service/store_service/StoreQueryServiceImpl.java
+++ spring/spring/src/main/java/umc/spring/service/store_service/StoreQueryServiceImpl.java
@@ -6,13 +6,16 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import umc.spring.apiPayload.code.status.ErrorStatus;
+import umc.spring.apiPayload.exception.handler.ReviewHandler;
 import umc.spring.apiPayload.exception.handler.StoreHandler;
+import umc.spring.converter.ReviewConverter;
 import umc.spring.domain.Mission;
 import umc.spring.domain.Review;
 import umc.spring.domain.Store;
 import umc.spring.repository.ReviewRepository;
 import umc.spring.repository.StoreRepository;
 import umc.spring.service.mission_service.MissionQueryService;
+import umc.spring.web.dto.store.StoreResponseDTO.ReviewDetailDTO;
 
 @Service
 @RequiredArgsConstructor
@@ -44,5 +47,12 @@ public class StoreQueryServiceImpl implements StoreQueryService {
     public Page<Mission> getMissionList(Long storeId, PageRequest pageRequest) {
         Store store = findById(storeId);
         return missionQueryService.findAllByStore(store, pageRequest);
+    }
+
+    @Override
+    public ReviewDetailDTO getReviewDetail(Long reviewId) {
+        Review review = reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new ReviewHandler(ErrorStatus.REVIEW_NOT_FOUND));
+        return ReviewConverter.toReviewDetailDTO(review);
     }
 }
