b444cefb3ab3c9e22960150e0c2a3cc4c3bac1a4
Minjae-An <mj3242@naver.com>
2024-01-19 14:18:33 +0900

    feat(ReviewConverter) : 리뷰 상세 응답 DTO, 리뷰 삭제 응답 DTO convert 로직 구현(#11)

--- spring/spring/src/main/java/umc/spring/converter/ReviewConverter.java
+++ spring/spring/src/main/java/umc/spring/converter/ReviewConverter.java
@@ -3,10 +3,13 @@ package umc.spring.converter;
 import static umc.spring.web.dto.store.StoreRequestDTO.AddReviewRequestDTO;
 import static umc.spring.web.dto.store.StoreResponseDTO.AddReviewResponseDTO;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 import umc.spring.domain.Review;
 import umc.spring.domain.ReviewImage;
+import umc.spring.web.dto.store.StoreResponseDTO.DeleteReviewDTO;
+import umc.spring.web.dto.store.StoreResponseDTO.ReviewDetailDTO;
 
 public class ReviewConverter {
     public static Review toReview(AddReviewRequestDTO request) {
@@ -17,7 +20,7 @@ public class ReviewConverter {
                 .build();
     }
 
-    public static ReviewImage toReviewImage(Review review, String imageUrl){
+    public static ReviewImage toReviewImage(Review review, String imageUrl) {
         return ReviewImage.builder()
                 .imageUrl(imageUrl)
                 .review(review)
@@ -38,6 +41,28 @@ public class ReviewConverter {
                 .storeId(review.getStore().getId())
                 .reviewId(review.getId())
                 .createdAt(review.getCreatedAt())
+                .build();
+    }
+
+    public static ReviewDetailDTO toReviewDetailDTO(Review review) {
+        List<String> reviewImageUrls = review.getReviewImages().stream()
+                .map(ReviewImage::getImageUrl)
+                .toList();
+
+        return ReviewDetailDTO.builder()
+                .reviewId(review.getId())
+                .wroteMemberId(review.getMember().getId())
+                .title(review.getTitle())
+                .reviewImageUrls(reviewImageUrls)
+                .body(review.getBody())
+                .score(review.getScore())
+                .build();
+    }
+
+    public static DeleteReviewDTO toDeleteReviewDTO(Review review) {
+        return DeleteReviewDTO.builder()
+                .storeId(review.getStore().getId())
+                .deletedAt(LocalDateTime.now())
                 .build();
     }
 }
